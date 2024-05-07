2a55d544e6ba7d320e01c119cebd31fe351242fd
benbousquet <bbousque@asu.edu>
2024-05-08 02:19:56 +0900

    added navbar navigation

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,24 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import KanjiPractice from "./kanjiPractice/kanjiPractice.tsx";
 import DeckViewList from "./deckList/deckListView.tsx";
 import Navbar from "./navigation/navbar.tsx";
 
+const NavbarLayout = () => (
+  <>
+    <header>
+      <Navbar />
+    </header>
+    <Outlet />
+  </>
+);
+
 const router = createBrowserRouter([
   {
-    path: "/study/:deckId",
-    element: <KanjiPractice />,
-  },
-  {
-    path: "/",
-    element: <DeckViewList />,
+    element: <NavbarLayout />,
+    children: [
+      {
+        path: "/study/:deckId",
+        element: <KanjiPractice />,
+      },
+      {
+        path: "/",
+        element: <DeckViewList />,
+      },
+    ],
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Navbar />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
--- frontend/src/navigation/navbar.tsx
+++ frontend/src/navigation/navbar.tsx
@@ -1,17 +1,24 @@
 import { Home, User } from "lucide-react";
 import { Button } from "../@/components/ui/button";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="h-20 w-full flex justify-between p-5">
-      <p className="text-3xl font-semibold tracking-tight transition-colors">Kanji Kit</p>
+      <p className="text-3xl font-semibold tracking-tight transition-colors">
+        Kanji Kit
+      </p>
       <div>
-        <Button variant="outline" size="icon" className="mx-1">
-          <User className="h-4 w-4" />
-        </Button>
-        <Button variant="outline" size="icon" className="mx-1">
-          <Home className="h-4 w-4" />
-        </Button>
+        <Link to="/login/">
+          <Button variant="outline" size="icon" className="mx-1">
+            <User className="h-4 w-4" />
+          </Button>
+        </Link>
+        <Link to="/">
+          <Button variant="outline" size="icon" className="mx-1">
+            <Home className="h-4 w-4" />
+          </Button>
+        </Link>
       </div>
     </div>
   );
