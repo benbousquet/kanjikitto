d3f7094d948a17dd5870d4ae78ff93805cf8a96b
benbousquet <bbousque@asu.edu>
2024-06-04 20:52:15 -0700

    added env variables

--- app/api/classify/route.ts
+++ app/api/classify/route.ts
@@ -2,7 +2,9 @@ export async function POST(request: Request) {
   const requestJSON = await request.json();
   const imgDataURL = requestJSON.imgDataURI;
 
-  const classify = await fetch("http://0.0.0.0:3001/classify", {
+  const MLApiURL = process.env.DEV === "true" ? process.env.ML_API_URL : "http://0.0.0.0:3001/classify"
+
+  const classify = await fetch(MLApiURL!, {
     method: "POST",
     headers: {
       Accept: "application/json",
