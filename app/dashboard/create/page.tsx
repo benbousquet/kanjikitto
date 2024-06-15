e8c71c9301868cfb253909cf462c9faa3a3638f6
benbousquet <bbousque@asu.edu>
2024-06-15 14:36:34 -0700

    added prisma and added pages

---
+++ app/dashboard/create/page.tsx
@@ -0,0 +1,16 @@
+export default function Create () {
+  return (
+    <div className="flex flex-col items-center">
+    <p>Create</p>
+      <div>
+        <p>form</p>
+      </div>
+      <div>
+        <p>items</p>
+      </div>
+      <div>
+        <p>here</p>
+      </div>
+    </div>
+  )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+export default function Dashboard () {
+  return (
+    <div className="flex flex-col items-center">
+    <p>Dashboard</p>
+      <div>
+        <p>created</p>
+      </div>
+      <div>
+        <p>saved</p>
+      </div>
+      <div>
+        <p>stats</p>
+      </div>
+    </div>
+  )
+}
