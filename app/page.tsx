a1b7e85cc94189b9d1ee73838d5d05f65efc186c
benbousquet <bbousque@asu.edu>
2024-06-15 20:20:24 -0700

    updated homescreen

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import { ReviewItem } from "@/components/types";
 import KanjiDrawing from "../components/kanjiDrawing";
-import { ArrowDown } from "lucide-react";
-export default function Home() {
+import { ArrowDown, Github, GraduationCap } from "lucide-react";
+import Link from "next/link";
+import { auth } from "@/auth";
+export default async function Home() {
+  const session = await auth();
+
   const exampleItem: ReviewItem = {
     hiragana: "きょう",
     kanji: "今日",
@@ -9,17 +13,43 @@ export default function Home() {
   };
   return (
     <main>
-      <div className="flex flex-col">
-        <h1 className="text-5xl text-center font-extrabold py-5 flex-wrap">
-          Start learning Kanji through writing!
-        </h1>
-        <h2 className="text-3xl text-center">
-          <span className="bg-yellow-700">Try it out below</span>
-        </h2>
-        <div className="flex flex-row justify-center animate-bounce">
-          <ArrowDown />
+      <div className="flex flex-col lg:flex-row max-w-fit lg:max-w-7xl mx-auto py-8 lg:py-24 snap-y snap-proximity">
+        <div className="max-w-2xl text-center lg:text-left lg:mr-16 snap-center">
+          <h1 className="text-5xl font-extrabold tracking-tight py-5 flex-wrap">
+            Learn Kanji 漢字 through writing!
+          </h1>
+          <p className="text-xl leading-relaxed py-2 px-3 lg:px-0">
+            Sometimes its hard to learn without doing, Kanji Kitto can help you
+            learn Kanji through writing with automatic feedback from AI!
+          </p>
+          <div className="my-5 lg:my-14">
+            <Link href={session ? "/dashboard" : "/api/auth/signin"} passHref={true}>
+            <button className="btn btn-primary btn-wide">
+              <GraduationCap />
+              Start learning
+            </button>
+            </Link>
+          </div>
+          <div className="divider"></div> 
+          <p className="text-xl leading-relaxed py-2 px-3 lg:px-0">
+            This project is open-source, if you are interested in contributing please checkout the projects GitHub.
+          </p>
+          <div className="my-5 lg:my-14">
+            <Link href="https://github.com/benbousquet/kanjikitto" passHref={true}>
+            <button className="btn btn-primary btn-wide" >
+              <Github />
+              GitHub
+            </button>
+            </Link>
+          </div>
         </div>
-        <KanjiDrawing word={exampleItem} nextItem={null} />
+        <div className="snap-center">
+          <h2 className="text-3xl text-center">Try it out</h2>
+          <div className="flex flex-row justify-center h-5 items-center">
+            <ArrowDown />
+          </div>
+          <KanjiDrawing word={exampleItem} nextItem={null} />
+        </div>
       </div>
     </main>
   );
