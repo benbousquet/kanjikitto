1fb6a250830b53b1432e79058a200b17033209a7
benbousquet <bbousque@asu.edu>
2024-06-19 15:48:28 -0700

    added deck list in dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,36 @@
-import { BarChartBig, Bookmark, History, Info, Search } from "lucide-react";
+import {
+  BarChartBig,
+  Bookmark,
+  CircleX,
+  History,
+  Info,
+  Search,
+  User,
+} from "lucide-react";
 import CreateButton from "./_components/createButton";
+import db from "@/lib/db";
+import { auth } from "@/auth";
+import Link from "next/link";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  // repeated code maybe consolidate into component later
+  const session = await auth();
+
+  if (!session || session === undefined) {
+    return (
+      <div className="flex flex-col items-center max-w-fit lg:max-w-7xl mx-auto py-8 lg:py-24 text-left">
+        <div role="alert" className="alert alert-error">
+          <CircleX />
+          <span>Error! You need to be logged in to access this page.</span>
+        </div>
+      </div>
+    );
+  }
+
+  const decks = await db.deck.findMany({
+    where: { authorId: session.user?.id },
+  });
+
   return (
     <div className="flex flex-col lg:max-w-7xl mx-auto py-8 lg:py-24">
       <div className="flex flex-col space-y-5 lg:space-y-0 lg:flex-row items-center lg:space-x-28">
@@ -68,6 +97,36 @@ export default function Dashboard() {
         </div>
         <div>
           <div className="flex flex-row items-center p-6 space-x-2">
+            <p className="text-2xl">My Decks</p>
+            <User className="text-primary" />
+          </div>
+          <div className="space-y-2 px-1 py-1">
+            {decks.map((deck) => {
+              return (
+                <div
+                  key={deck.id}
+                  tabIndex={0}
+                  className="collapse bg-base-200"
+                >
+                  <div className="collapse-title text-xl font-medium flex flex-row justify-between items-center">
+                    {deck.title}
+                    <Link
+                      className="btn btn-primary"
+                      href={"/deck/review/" + deck.id}
+                    >
+                      Study
+                    </Link>
+                  </div>
+                  <div className="collapse-content content-end">
+                    <p>{deck.description}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        {/* <div>
+          <div className="flex flex-row items-center p-6 space-x-2">
             <p className="text-2xl">Stats</p>
             <BarChartBig className="text-primary" />
           </div>
@@ -84,7 +143,7 @@ export default function Dashboard() {
               <div className="stat-desc">↗︎ 400 (22%)</div>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
